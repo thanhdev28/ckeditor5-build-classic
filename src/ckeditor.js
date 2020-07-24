6e27560fcb53770f337a354bab9002251da703cf
Thanhdc <thanhdinhcong@admicro.vn>
2020-07-24 17:51:48 +0700

    update feature

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -29,7 +29,8 @@ import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefrom
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
-
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
+import Mention from '@ckeditor/ckeditor5-mention/src/mention';
 export default class ClassicEditor extends ClassicEditorBase {}
 
 // Plugins to include in the build.
@@ -56,7 +57,9 @@ ClassicEditor.builtinPlugins = [
 	PasteFromOffice,
 	Table,
 	TableToolbar,
-	TextTransformation
+	TextTransformation,
+	Alignment,
+	Mention
 ];
 
 // Editor configuration.
@@ -65,6 +68,7 @@ ClassicEditor.defaultConfig = {
 		items: [
 			'heading',
 			'|',
+			'alignment',
 			'bold',
 			'italic',
 			'link',
